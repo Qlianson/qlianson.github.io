05db83fce59de3d0d146c2a8a004f34043c3c0fd
Alex fellow <myemail@mail.com>
2025-06-30 01:26:05 +0300

    Change address popup added

---
+++ app/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const popup = document.querySelector('#address-popup'),
+          checkbox = document.querySelector('#address-checkbox'),
+          button = document.querySelector('#address-btn'),
+          STORAGE_KEY = 'addressChecked',
+          ONE_DAY_MS = 24 * 60 * 60 * 1000,
+          now = Date.now();
+
+    if (!localStorage.getItem(STORAGE_KEY) || now - Number(localStorage.getItem(STORAGE_KEY)) > ONE_DAY_MS) {
+
+        popup.classList.remove('hidden');
+        document.body.style.overflow = 'hidden';
+    }
+
+    checkbox.addEventListener('change', () => {
+
+        if (checkbox.checked) {
+            button.classList.remove('disable-btn');
+            button.classList.add('button_main');
+        } else {
+            button.classList.remove('button_main');
+            button.classList.add('disable-btn');
+        }
+
+    })
+
+    button.addEventListener('click', () => {
+
+        if (button.classList.contains('button_main')) {
+            localStorage.setItem(STORAGE_KEY, Date.now().toString());
+            popup.classList.add('hidden');
+            document.body.style.overflow = '';
+
+        }
+
+    })
+})
