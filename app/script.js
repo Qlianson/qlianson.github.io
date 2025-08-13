d8dbbac4da790d90180227850ea97bf0c3a61e98
Alex fellow <myemail@mail.com>
2025-08-13 13:00:10 +0300

    remove check timer

--- app/script.js
+++ app/script.js
@@ -2,18 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
     const popup = document.querySelector('#address-popup'),
           checkbox = document.querySelector('#address-checkbox'),
           button = document.querySelector('#address-btn'),
-          STORAGE_KEY = 'addressChecked',
-          DAYS_DELAY = 10 * (24 * 60 * 60 * 1000), // 10 days
-          now = Date.now();
+          STORAGE_KEY = 'addressChecked'
 
-    if (!localStorage.getItem(STORAGE_KEY) || now - Number(localStorage.getItem(STORAGE_KEY)) > DAYS_DELAY) {
-
+    if (!localStorage.getItem(STORAGE_KEY)) {
         popup.classList.remove('hidden');
         document.body.style.overflow = 'hidden';
     }
 
     checkbox.addEventListener('change', () => {
-
         if (checkbox.checked) {
             button.classList.remove('disable-btn');
             button.classList.add('button_main');
@@ -21,17 +17,13 @@ window.addEventListener('DOMContentLoaded', () => {
             button.classList.remove('button_main');
             button.classList.add('disable-btn');
         }
-
     })
 
     button.addEventListener('click', () => {
-
         if (button.classList.contains('button_main')) {
-            localStorage.setItem(STORAGE_KEY, Date.now().toString());
+            localStorage.setItem(STORAGE_KEY, '');
             popup.classList.add('hidden');
             document.body.style.overflow = '';
-
         }
-
     })
 })
